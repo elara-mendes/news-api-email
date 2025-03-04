a3e262bf1c0c892e86821eeee7047ee939764f0c
Elara <elaramendes@yahoo.com>
2025-03-04 16:36:34 -0300

    Improved an old script that send mails to send news

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import itertools
+from send_email import email_send
 
 NEWS_API = os.getenv("NEWS_API")
 
@@ -7,6 +9,8 @@ url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-02-04&sortBy=publish
 request_info = requests.get(url)
 content = request_info.json()
 
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+for article in itertools.islice(content["articles"], 1):
+    article_title = article["title"]
+    article_description = article["description"]
+    email_send(article_title, article_description)
+    print(article_title, article_description)
